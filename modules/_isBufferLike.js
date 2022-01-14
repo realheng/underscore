68bc3babf6b30a9defbff5bd5cd4c8a33a892533
RealHeng <596484539@qq.com>
2022-01-14 23:20:12 +0800

    comment

--- modules/_isBufferLike.js
+++ modules/_isBufferLike.js
@@ -1,6 +1,6 @@
-import createSizePropertyCheck from './_createSizePropertyCheck.js';
-import getByteLength from './_getByteLength.js';
+import createSizePropertyCheck from './_createSizePropertyCheck.js'
+import getByteLength from './_getByteLength.js'
 
 // Internal helper to determine whether we should spend extensive checks against
 // `ArrayBuffer` et al.
-export default createSizePropertyCheck(getByteLength);
+export default createSizePropertyCheck(getByteLength)
